51eec731df9edc02c5bdaa23bb3312133d005fcb
vihisantos <vitrsv462@gmail.com>
2025-12-31 04:20:22 -0300

    build: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/My.Portfolio/",
   server: {
     host: "::",
     port: 8080,
