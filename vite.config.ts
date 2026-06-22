97d6d91670c5eedbfa971580abd70b635df97b0e
vihisantos <vitrsv462@gmail.com>
2026-06-22 05:06:51 -0300

    fix: restore base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/My.Portfolio/",
   server: {
     host: "::",
     port: 8080,
