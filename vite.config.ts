8bc81f4f2d04bc249bc6637653903779d72e1541
vihisantos <vitrsv462@gmail.com>
2026-06-22 04:45:05 -0300

    fix: change base path to root for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/My.Portfolio/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
